d57b688768b354983c8b0064a0cef4f7a6d38b78
marcin wawer <m.wawer.git@icloud.com>
2025-05-18 19:22:59 +0200

    chore: project setup

---
+++ GitHubFollowers/FirstScreen.swift
@@ -0,0 +1,20 @@
+//
+//  FirstScreen.swift
+//  GitHubFollowers
+//
+//  Created by Marcin Wawer on 18-05-2025.
+//
+
+import UIKit
+
+class FirstScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
