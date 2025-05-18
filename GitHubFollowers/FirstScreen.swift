664e8468b2feb38848346e7bb4b4a08f57b395f5
marcin wawer <m.wawer.git@icloud.com>
2025-05-18 19:54:23 +0200

    feat: add assets
    Adds images for app icon, avatar placeholder, empty state and logo, and Github logo for light and dark mode.

--- GitHubFollowers/FirstScreen.swift
+++ GitHubFollowers/FirstScreen.swift
@@ -12,7 +12,6 @@ class FirstScreen: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemMint
-        // Do any additional setup after loading the view.
     }
 
 
